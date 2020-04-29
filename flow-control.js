309c8e5f1e0a9895e032e05bebada8cbecc04ff5
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-04-29 07:21:24 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -9,7 +9,7 @@ function teenager(age) {
   if( age >= 13 && age <= 19){
   return 'You are a teenager!'
 } else {
-    return 'You are not a teenage'
+    return 'You are not a teenager'
   }
 
 }
@@ -31,5 +31,31 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
+  switch(age){
+    case 13:
+    console.log("You are a teenager");
+    break;
+    case 14:
+    console.log("You are a teenager");
+    break;
+    case 15:
+    console.log("You are a teenager");
+    break;
+    case 16:
+    console.log("You are a teenager");
+    break;
+    case 17:
+    console.log("You are a teenager");
+    break;
+    case 18:
+    console.log("You are a teenager");
+    break;
+    case 19:
+    console.log("You are a teenager");
+    break;
+    default:
+    console.log("You have an age");
+  }
+  
 
 }
